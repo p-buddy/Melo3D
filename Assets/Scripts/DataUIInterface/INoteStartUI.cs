b1d33366f1a4bb8841cac896a960d519b0f7fdc9
p-buddy <pmalacho@usc.edu>
2021-08-21 16:17:20 -0400

    Setting up docfx

--- Assets/Scripts/DataUIInterface/INoteStartUI.cs
+++ Assets/Scripts/DataUIInterface/INoteStartUI.cs
@@ -3,6 +3,9 @@ using Unity.Mathematics;
 
 namespace DataUIInterface
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public interface INoteStartUI : IUIComponent, ISettableFromCodeUI<float2>, IRetrievableUI<float2>, IUserEditableUI<float2>
     {
         
